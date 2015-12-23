6171d8d4682366ab92ffcd702089e21a1aa24df2
xSplit <rophp@live.it>
2015-12-23 15:28:29 +0100

    Update example.php

--- example.php
+++ example.php
@@ -6,10 +6,9 @@ $steam->setReturnUrl('example.php');
 if($steam->verifyLogin()) {
     $steam->setSteamKey('API_KEY'); //YOU CAN GET THIS HERE https://steamcommunity.com/dev/apikey
     $user = $steam->loadProfile();
-
-    echo '<pre>'.print_r($user,true).'</pre>';
-
     $_SESSION['user'] = $user->personaname;
+    
+    echo '<pre>'.print_r($user,true).'</pre>';
 }else if(!isset($_SESSION['user']))
     echo $steam->getAuthButton();
 
